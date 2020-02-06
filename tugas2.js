566ea14690324af783391079e2cbe4b182f66a0a
jovanpangestu <pangestujovan25@gmail.com>
2020-02-06 09:34:30 +0800

    Tugas_2_Javascript
    tugas ini saya kumpulkan untuk NIOMIC

---
+++ tugas2.js
@@ -0,0 +1,16 @@
+var number = 100;
+var string = "Assalamualaikum";
+var cm = 167;
+if (cm>165)
+	tinggi = true;
+else
+	tinggi = false;
+var test = "kosong";
+test = null;
+var array = [1,2,3,4,5,6];
+
+console.log(number)
+console.log(string)
+console.log(tinggi)
+console.log(test)
+console.log(array)
